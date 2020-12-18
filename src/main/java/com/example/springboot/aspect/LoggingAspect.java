437e04060fff4539d7a5642222ff4c05db681e49
Jeevan Maben <jeevanmaben@Jeevans-MacBook-Pro.local>
2020-12-18 10:33:04 +0530

    adding after return

--- src/main/java/com/example/springboot/aspect/LoggingAspect.java
+++ src/main/java/com/example/springboot/aspect/LoggingAspect.java
@@ -1,12 +1,15 @@
 package com.example.springboot.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 @Component
 @Aspect
@@ -20,6 +23,18 @@ public class LoggingAspect {
     public void logMethodExecution(JoinPoint jointPoint){
         StringBuilder message = new StringBuilder("Method: ");
         message.append(jointPoint.getSignature().getName());
+        LOGGER.info(message.toString());
+    }
+
+    @AfterReturning(value = "executeLogging()", returning = "returnValue")
+    public void logMethodReturn(JoinPoint jointPoint, Object returnValue){
+        StringBuilder message = new StringBuilder("After returning from: ");
+        message.append(jointPoint.getSignature().getName());
+        if (returnValue instanceof Collection){
+            message.append(" Return value::" + ((Collection<?>) returnValue).size());
+        }else{
+            message.append(" Return value::" + returnValue.toString());
+        }
         LOGGER.info(message.toString());
     }
 }
